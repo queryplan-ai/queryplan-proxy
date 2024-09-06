feb6a6b303ca9c5ada517b8c880bd666b6e086c9
Marc Campbell <marc.e.campbell@gmail.com>
2024-09-06 05:32:28 -0700

    update dev env, send prepared statements (part 1) (#6)
    * Add okteto dev env
    
    * Move dockerfile
    
    * add kustomization
    
    * rename file
    
    * Adding env from
    
    * remove the limits on dev pod
    
    * Add service
    
    * port 3306
    
    * work on dev env
    
    * roughly adding in prepared statemetns

--- pkg/heartbeat/heartbeat.go
+++ pkg/heartbeat/heartbeat.go
@@ -1,16 +1,46 @@
 package heartbeat
 
 import (
+	"strings"
 	"time"
 
 	"github.com/queryplan-ai/queryplan-proxy/pkg/heartbeat/types"
 )
 
 func AddPendingQuery(query string) {
+	// some queries we filter here
+	if isFilteredQuery(query) {
+		return
+	}
+
 	qpq := types.QueryPlanQuery{
 		Query:      query,
 		ExecutedAt: time.Now().UnixNano(),
 	}
 
 	pendingQueries.Add(qpq)
+}
+
+func isFilteredQuery(query string) bool {
+	if strings.ToLower(query) == "select ?" {
+		return true
+	}
+
+	if strings.ToLower(query) == "begin transaction" {
+		return true
+	}
+
+	if strings.ToLower(query) == "commit" {
+		return true
+	}
+
+	if strings.ToLower(query) == "rollback" {
+		return true
+	}
+
+	if strings.ToLower(query) == "SELECT @@max_allowed_packet" {
+		return true
+	}
+
+	return false
 }
--- pkg/mysql/command.go
+++ pkg/mysql/command.go
@@ -1,22 +1,52 @@
 package mysql
 
 import (
+	"encoding/binary"
 	"fmt"
 	"io"
 	"log"
 	"net"
 	"strings"
 
+	"github.com/pkg/errors"
 	"github.com/queryplan-ai/queryplan-proxy/pkg/heartbeat"
+	"github.com/queryplan-ai/queryplan-proxy/pkg/ringbuffer"
 )
 
 var (
 	ErrNonQueryData                   = fmt.Errorf("non-query data")
 	ErrNonQueryDataOrIncompletePacket = fmt.Errorf("non-query data or incomplete packet")
+	ErrUnknownPreparedStatement       = fmt.Errorf("unknown prepared statement")
+)
+
+type PreparedStatement struct {
+	ID    int
+	Query string
+}
+
+var (
+	preparedStatements = ringbuffer.New[PreparedStatement](1000)
 )
 
 const (
-	COM_QUERY = 0x03
+	COM_QUIT             = 0x01
+	COM_INIT_DB          = 0x02
+	COM_QUERY            = 0x03
+	COM_FIELD_LIST       = 0x04
+	COM_CREATE_DB        = 0x05
+	COM_DROP_DB          = 0x06
+	COM_REFRESH          = 0x07
+	COM_STATISTICS       = 0x09
+	COM_PROCESS_INFO     = 0x0a
+	COM_CONNECT          = 0x0b
+	COM_PROCESS_KILL     = 0x0c
+	COM_DEBUG            = 0x0d
+	COM_PING             = 0x0e
+	COM_CHANGE_USER      = 0x11
+	COM_RESET_CONNECTION = 0x1f
+	COM_STMT_PREPARE     = 0x16
+	COM_STMT_EXECUTE     = 0x17
+	COM_STMT_CLOSE       = 0x19
 )
 
 func copyAndInspectCommand(src, dst net.Conn, inspect bool) error {
@@ -40,6 +70,10 @@ func copyAndInspectCommand(src, dst net.Conn, inspect bool) error {
 				} else {
 					heartbeat.AddPendingQuery(cleanedQuery)
 				}
+			} else {
+				if errors.Cause(err) != ErrNonQueryData {
+					log.Printf("Error extracting query: %v", err)
+				}
 			}
 		}
 
@@ -56,9 +90,41 @@ func extractQuery(data []byte) (string, error) {
 	if len(data) < 5 {
 		return "", ErrNonQueryDataOrIncompletePacket
 	}
-	if data[4] == COM_QUERY {
-		fmt.Printf("data: %v\n", data)
+
+	switch data[4] {
+	case COM_QUERY:
 		return strings.TrimSpace(string(data[5:])), nil
+	case COM_STMT_PREPARE:
+		query := strings.TrimSpace(string(data[5:]))
+		preparedStatements.Add(PreparedStatement{
+			ID:    -1, // we don't have the ID yet, we will find this in the response protocol from the server
+			Query: query,
+		})
+		return query, nil // really this should be after we find the statement in COM_STMT_EXECUTE
+	case COM_STMT_EXECUTE:
+		// find the prepared statement with the same id, log that this query was executed
+		if len(data) < 9 {
+			return "", ErrNonQueryDataOrIncompletePacket
+		}
+		stmtID := binary.LittleEndian.Uint32(data[5:9])
+		for _, ps := range preparedStatements.GetAll() {
+			if ps.ID == int(stmtID) {
+				// we don't have a function to remove an item from our ringbuffer
+				// so we let it expire
+				return ps.Query, nil
+			}
+		}
+
+		// fmt.Printf("Unknown prepared statement ID: %v\n", stmtID)
+		return "", ErrUnknownPreparedStatement
+
+	case COM_QUIT, COM_INIT_DB, COM_FIELD_LIST, COM_CREATE_DB,
+		COM_DROP_DB, COM_REFRESH, COM_STATISTICS, COM_PROCESS_INFO,
+		COM_CONNECT, COM_PROCESS_KILL, COM_DEBUG, COM_PING,
+		COM_CHANGE_USER, COM_RESET_CONNECTION, COM_STMT_CLOSE:
+		return "", ErrNonQueryData
 	}
+
+	fmt.Printf("Unknown command: %v\n", data[4])
 	return "", ErrNonQueryData
 }
