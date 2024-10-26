dd3dbbd83f18c85902ef5542b9bf3ef9c6b3c8a7
Marc Campbell <marc.e.campbell@gmail.com>
2024-10-26 06:11:46 -0700

    Some little fixes for cleaning queries and prepared statements

--- pkg/postgres/clean.go
+++ pkg/postgres/clean.go
@@ -8,7 +8,18 @@ import (
 )
 
 func cleanQuery(query string) (string, error) {
+	// remove the \x00 control characters from the start and end of the query
+
+	for strings.HasPrefix(query, "\x00") {
+		query = query[1:]
+	}
+
+	for strings.HasSuffix(query, "\x00") {
+		query = query[:len(query)-1]
+	}
+
 	query = strings.TrimSpace(query)
+
 	fmt.Printf("Query: %q\n", query)
 	cleanedQuery, err := obfuscate.NewObfuscator(obfuscate.Config{
 		SQL: obfuscate.SQLConfig{
